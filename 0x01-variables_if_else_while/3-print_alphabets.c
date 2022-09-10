ec81f6adb04d39aacbfadd4730eee3d6612c4187
DT-GAMER <abakpad82@gmail.com>
2022-09-10 16:40:23 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+putchar(alpha);
+putchar('\n');
+
+return (0);
+}
