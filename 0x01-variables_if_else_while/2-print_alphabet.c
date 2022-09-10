c90067f23fafd46736e42d274c56a67ffbbad047
DT-GAMER <abakpad82@gmail.com>
2022-09-10 16:07:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entty point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char alpha;
+for ("alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+putchar('\n');
+
+return (0);
+}
