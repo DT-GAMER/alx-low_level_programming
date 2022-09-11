d586d4952cab3619bc20f0d125d063df0b4b8a94
DT-GAMER <abakpad82@gmail.com>
2022-09-11 14:06:21 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int d;
+
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+if (d != '9')
+putchar(','");
+putchar(' ');
+
+putchar('\n');
+
+return (0);
+}
