032fb42fc41690888be867bf5c0ed9fc849c0ca5
DT-GAMER <abakpad82@gmail.com>
2022-09-11 06:21:55 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char alpha;
+
+for (alpha = 'z'; alpha <= 'a'; alpha--)
+putchar(alpha);
+putchar('\n');
+
+return (0);
+}
