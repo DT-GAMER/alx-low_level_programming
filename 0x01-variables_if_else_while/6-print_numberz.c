76b8d1cc92be8940c5a3ff1db92dd0a1cd1f63e9
DT-GAMER <abakpad82@gmail.com>
2022-09-11 05:03:18 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int d;
+
+for (d = '0'; d < '10'; d++)
+putchar(d);
+putchar('\n');
+
+return (0);
+}
