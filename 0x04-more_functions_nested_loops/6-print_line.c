45ec73f32cd32989d93a64f9def6fcce4463f2dc
DT-GAMER <abakpad82@gmail.com>
2022-09-15 13:40:54 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_lines - print straight line n times
+ * print_line - print straight line n times
  * @n: character value to be executed
  * Return: void
  */
-void print_lines(int n)
+void print_line(int n)
 {
-int z;
-for (z = '0'; z < n; z++)
+int i;
+for (i = '0'; i < n; i++)
 {
 _putchar('_');
 }
