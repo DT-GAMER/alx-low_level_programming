a23525941f63791e0a5cc8ae3e93b70e365872a7
Abakpa Dominic <abakpad82@gmail.com>
2022-09-18 01:43:11 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
-#include <stdio.h> 
- #include "holberton.h" 
   
- /** 
-  * main - entry point 
-  * 
-  * Description: prints the numbers 1 - 100 w/ 
-  * Fizz for multiples of 3, Buzz for multiples of 5, 
-  * and FizzBuzz for multiples of both 
-  * 
-  * Return: always 0 
-  */ 
- int main(void) 
- { 
+/** 
+ * main - entry point 
+ * 
+ * Description: prints the numbers 1 - 100 w/   * Fizz for multiples of 3, Buzz for multiples of 5, 
+ * and FizzBuzz for multiples of both 
+ * 
+ * Return: always 0 
+ */ 
+int main(void) 
+{ 
          int i; 
   
          for (i = 1; i <= 100; i++) 
@@ -32,4 +28,4 @@
          } 
          printf("\n"); 
          return (0); 
- }
+}
