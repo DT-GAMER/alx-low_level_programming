7dea65cdce8228999a6c32e8655bd803bb84ea13
DT-GAMER <abakpad82@gmail.com>
2022-09-19 08:36:34 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-
-/**
- *main - starting point
- *
- *return 0  
- */
-int main(
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+  puts("Programming is like building a multilingual puzzle\n");
   return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,6 @@
-#include <stdio.h>
-
-/** 
-  * main - Entry point 
-  * 
-  * Return: Always 0 (Success) 
-  */
-int main(void) 
+#include <stdio.h>/
+int main 
 {
-          printf("with proper grammar, but the outcome is a piece of art,\n"); 
-          return (0); 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); 
 }
