c5aa77f2c55b1a7fbfad62d7a16954b029654ad0
DT-GAMER <abakpad82@gmail.com>
2022-09-19 16:22:19 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2_strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * strlen_int - A function that prints the length of a string
+ * @s: String to find length
+ * Return: void
+ */
+
+void _strlen(int *s)
+{
+int n = 0;
+
+while (*(s + n))
+n++;
+return (i);
+}
