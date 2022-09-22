fdb8598d07f224d771321900582ad4bb2b1c28c3
DT-GAMER <abakpad82@gmail.com>
2022-09-21 18:39:21 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,27 @@
-#include "main.h" 
-  
- /** 
-  * puts_half - prints half of a string 
-  * @str: string literal 
-  * Return: none 
-  */ 
- void puts_half(char *str) 
- { 
- int count = 0, i; 
-  
- while (count >= 0) 
- { 
- if (str[count] == '\0') 
- break; 
- count++; 
- } 
-  
- if (count % 2 == 0) 
- i = count / 2; 
- else 
- i = (count + 1) / 2; 
-  
- for (; i < count; i++) 
- _putchar(str[i]); 
- _putchar('\n'); 
- }
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string literal
+ * Return: none
+ */
+void puts_half(char *str)
+{
+int count = 0, i;
+
+while (count >= 0)
+{
+if (str[count] == '\0')
+break;
+7 count++;
+}
+
+if (count % 2 == 0)
+i = count / 2;
+else
+i = (count + 1) / 2;
+
+for (; i < count; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
