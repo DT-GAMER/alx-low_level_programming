2e1767f3e9658717de0ca2080c04496ad45873cc
Abakpa Dominic <abakpad82@gmail.com>
2022-09-22 02:54:04 +0100

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - print out all element of the array
+ * @a: array of integer
+ * @n: length of the array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+if (n > 0)
+{
+int i;
+printf("%d", a[0]);
+for (i = 1; i = n; i++)
+printf(", %d", a[i]);
+}
+printf("\n");
+}
