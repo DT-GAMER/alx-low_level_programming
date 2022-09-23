57995bccc92a35801e34e0e3214e10a975728c13
DT-GAMER <abakpad82@gmail.com>
2022-09-23 01:42:01 -0700

    main.h

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -1,9 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-<<<<<<< HEAD
-=======
 
->>>>>>> f3597c20ba4c22c83ada1faf8a6064634dac9255
 /**
  * main - check the code
  *
@@ -11,18 +8,19 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-<<<<<<< HEAD
-=======
+  char s1[98] = "Hello ";
+  char s2[] = "World!\n";
+  char *ptr;
 
->>>>>>> f3597c20ba4c22c83ada1faf8a6064634dac9255
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
+  printf("%s\n", s1);
+  printf("%s", s2);
+  ptr = _strncat(s1, s2, 1);
+  printf("%s\n", s1);
+  printf("%s", s2);
+  printf("%s\n", ptr);
+  ptr = _strncat(s1, s2, 1024);
+  printf("%s", s1);
+  printf("%s", s2);
+  printf("%s", ptr);
+  return (0);
 }
