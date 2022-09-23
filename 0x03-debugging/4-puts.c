b74b2216e8cfef14e999c09d367e7f35ffe8aabf
Abakpa Dominic <abakpad82@gmail.com>
2022-09-23 12:17:16 +0100

    4-puts.c

---
+++ 0x03-debugging/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string
+ * Return: 0 after printing to the standard output
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
