b88bf5c8b6edcc7d32da247fe4e466adde4b4a86
DT-GAMER <abakpad82@gmail.com>
2022-12-16 09:21:48 -0800

    not_random.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp/ https://raw.github.com/DT-GAMER/alx-low_level_programming/masters/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+curl https://raw.githubusercontent.com/DT-GAMER/alx-low_level_programming/master/0x18-dynamic_libraries/not_random.so -o /tmp/not_random.so
+export LD_PRELOAD=/tmp/not_random.so:$LD_PRELOAD
