965b4e77aeb0a0ead6111c3a8092799925f2e5f8
Abakpa Dominic <abakpad82@gmail.com>
2023-01-06 18:43:44 +0100

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,19 +1,16 @@
 #include "hash_tables.h"
 
 /**
-
- * key_index - generates the key using hash_djb2
-
- * @key: pointer the key
-
- * @size: size table
-
- * Return: index key
-
+ * key_index - Associate a index to a key.
+ * @key: Key to be indexed.
+ * @size: Size of the hash table.
+ *
+ * Return: Integer after the operations.
  */
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-
 {
+	unsigned long int result;
 
-	return (hash_djb2(key) % size);}
+	result = hash_djb2(key) % size;
+	return (result);
+}
