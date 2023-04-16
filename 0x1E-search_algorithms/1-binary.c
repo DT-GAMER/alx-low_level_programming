82e2ef077c96c71d424902ca04edb649c01563f3
Abakpa Dominic <abakpad82@gmail.com>
2023-04-16 15:08:11 +0100

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,9 +1,10 @@
 #include "search_algos.h"
+
 /**
 * pri_arr - prints array
-* array = array
-* start= start point
-* end = ending point
+* @array: array
+* @start: start point
+* @end: ending point
 * Return nothing
 **/
 void pri_arr(int *array, size_t start, size_t end)
@@ -19,41 +20,41 @@ void pri_arr(int *array, size_t start, size_t end)
 }
 
 /**
-* binary_search - a function that searches for a value in a
-* sorted array of integers using the Binary search algorithm
-* array: pointer to the first element of the array
-* size: is the number of elements in array
-* value: value to search for
-* Return: index OR -1
-**/
+ * binary_search - function that searches fror a value in a
+ * sorted array of integers using the binary search algorithm
+ * @array: pointer to first element of the array
+ * @size: number of elements in the array
+ * @value: element to search for
+ * Return: -1 if value is absent or array is null,
+ * index of value
+ */
+
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned int left = 0, right;
+	unsigned int ist = 0;
+	unsigned int last = size - 1;
 	int middle;
-
-	right = size - 1;
 
 	if (array == NULL)
 		return (-1);
 	if (size == 1)
+		return (array[ist]);
+
+	while (ist <= last)
 	{
-		return (array[left]);
-	}
-	while (right >= left)
-	{
-		if (left == 0 && right == 0)
+		if (ist == 0 && last == 0)
 			return (-1);
-		pri_arr(array, left, right);
-		middle = (right + left) / 2;
+		pri_arr(array, ist, last);
+		middle = (ist + last) / 2;
 		if (array[middle] == value)
 			return (array[middle]);
 		else if (array[middle] < value)
 		{
-			left = middle + 1;
+			ist = middle + 1;
 		}
 		else if (array[middle] > value)
 		{
-			right = middle;
+			last = middle - 1;
 		}
 	}
 	return (-1);
